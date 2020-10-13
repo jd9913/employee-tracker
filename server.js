304565751e05f43d9a2ec8577830ee44cf37a5d9
Jennifer Schlierman <jd9913@gmail.com>
2020-10-12 20:14:13 -0700

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const sqlite3 = require('sqlite3').verbose();
 
 const express=require('express');
 
-const PORT = process.env.PORT||3001;
+const PORT = process.env.PORT||3011;
 const app = express();
 
 app.use(express.urlencoded({ extended: false}));
@@ -24,6 +24,35 @@ const db = new sqlite3.Database('./db/tracker.db', err => {
 
 
 
+
+
+
+
+
+  
+
+
+// Delete a candidate
+db.run(`DELETE FROM department WHERE id = ?`, 1, function(err, result) {
+  if (err) {
+    console.log(err);
+  }
+  console.log(result, this, this.changes);
+});
+
+
+// // GET a single department
+// db.get(`SELECT * FROM department WHERE id = 1`, (err, row) => {
+//     if(err) {
+//       console.log(err);
+//     }
+//     console.log(row);
+//   });
+
+
+//   db.all(`SELECT * FROM department`, (err, rows) => {
+//     console.log(rows);
+//   });
 
 // Default response for any other requests(Not Found) Catch all
 app.use((req, res) => {
